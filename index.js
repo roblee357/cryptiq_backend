f468aff1925a7a707a68f7dd8ca2ccaf8a18e0f9
roblee357 <roblee357@hotmail.com>
2021-09-26 09:24:24 -0400

    port to 443

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ app.get('/', (req, res) => {
         });
   });
   
-  server.listen(8000, () => {
-    console.log('listening on *:8000');
+  server.listen(443, () => {
+    console.log('listening on *:443');
   });
